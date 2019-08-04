dcff3dfcc9e1207cac010e8b79193441594a1552
EXTENSION <luis.arizar@hotmail.com>
2019-08-04 14:05:00 -0500

    mejora

---
+++ Cap14/slnApp/App.UI.WebForm/Content/JS/JavaScript.js
@@ -0,0 +1,48 @@
+﻿debugger;
+
+var numero = 15;
+var cadeno = "Hola Javascript";
+var booleanTrue = true;
+var booleanFalse = false;
+var date = new Date();
+var date2 = new Date(2017,01,15);
+var alumno = {
+    Nombre: "Juan",
+    Apellidos: "Días",
+    FechaNacimiento: "1980/05/04",
+    GenerarNombreCompleto: function (genero) {
+        var fullName = this.Nombre + " " + this.Apellidos; 
+        debugger;
+        if (genero = "M") {
+            fullName = "Señor: " + fullName;
+        }
+        else {
+            fullName = "Señora: " + fullName;
+        }
+
+        return fullName;
+    }
+};
+
+//arreglo
+var clase = [];
+//agregando elementos a un areglo
+clase.push(alumno);
+
+
+
+
+
+
+
+
+function MostrarAlumno() {  
+    //Mostrar Mensaje
+    //alert("Es el alumno:" + alumno.GenerarNombreCompleto("M"));
+
+    for (var i = 0; i < clase.length; i++) {
+        alert("Es el alumno:" + clase[i].GenerarNombreCompleto());
+    }
+
+
+}
